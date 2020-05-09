4e1e07c97f4b879462b3b90afae5679b3315e593
Max Friedrich <2mfriedr@informatik.uni-hamburg.de>
2020-05-09 18:19:29 +0200

    Remove fixed random seed

--- app/models/Shuffle.scala
+++ app/models/Shuffle.scala
@@ -3,12 +3,12 @@ package models
 import scala.util.Random
 
 object Shuffle {
-  Random.setSeed(1L)
+  private val random = new Random()
 
   case class ShuffleResult(playerCards: Seq[Seq[Card]], deck: Seq[Card], pile: Pile)
 
-  def shuffle(numPlayers: Int, playerNumCards: Int, allCards: Seq[Card]): ShuffleResult = {
-    val shuffledDeck = Random.shuffle(allCards)
+  def shuffle(numPlayers: Int, playerNumCards: Int, allCards: Seq[Card], random: Random = random): ShuffleResult = {
+    val shuffledDeck = random.shuffle(allCards)
     val playerCards  = (0 until numPlayers).map(i => shuffledDeck.slice(i * playerNumCards, (i + 1) * playerNumCards))
     val pileTop      = shuffledDeck.slice(numPlayers * playerNumCards, numPlayers * playerNumCards + 1).head
     val deck         = shuffledDeck.drop(numPlayers * playerNumCards + 1)
@@ -18,9 +18,9 @@ object Shuffle {
 
   case class ReshuffleResult(deck: Seq[Card], pile: Pile)
 
-  def reshuffle(pile: Pile): ReshuffleResult = {
+  def reshuffle(pile: Pile, random: Random = random): ReshuffleResult = {
     val newPile = Pile(pile.top, Seq.empty, Seq.empty)
-    val newDeck = Random.shuffle(pile.drawable.map(_.card) ++ pile.bottom)
+    val newDeck = random.shuffle(pile.drawable.map(_.card) ++ pile.bottom)
     ReshuffleResult(newDeck, newPile)
   }
 
