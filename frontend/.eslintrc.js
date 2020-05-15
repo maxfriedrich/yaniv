5c52e990171ad95022bf7e83f280d42f36a07ef2
Max Friedrich <2mfriedr@informatik.uni-hamburg.de>
2020-05-15 23:23:22 +0200

    Prepare typescript

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    project: "./tsconfig.eslint.json",
+  },
+  rules: {
+    "react/no-unknown-property": ["error", { ignore: ["class"] }],
+  },
+  settings: {
+    react: {
+      pragma: "h",
+      version: "detect"
+    },
+  },
+};
---
+++ frontend/webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/app',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loaders: ['ts-loader']
+      }
+    ]
+  }
+}
