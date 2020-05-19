09e4c0f2592128ec3a7530fa06a74a571d8dd66d
Max Friedrich <2mfriedr@informatik.uni-hamburg.de>
2020-05-19 23:26:28 +0200

    Revert commented out Yaniv activation debug

--- frontend/src/components/game/index.tsx
+++ frontend/src/components/game/index.tsx
@@ -261,8 +261,8 @@ export class Game extends Component<
   isThrowDisabled = () =>
     !this.isCurrentPlayerThrow() || this.state.selected.length === 0;
   isYanivDisabled = () =>
-    !this.isCurrentPlayerThrow() || this.state.selected.length > 0 || false;
-  // this.state.sortedCards.map(c => c.endValue).reduce((acc, x) => acc + x) > 5;
+    !this.isCurrentPlayerThrow() || this.state.selected.length > 0 ||
+    this.state.sortedCards.map(c => c.endValue).reduce((acc, x) => acc + x) > 5;
 
   drawFromPile = (card: CardType) => {
     console.log('Drawing from pile: ', card.id);
